6bf33279ceb4f0bb1c0729d6106da87a4faf8e25
William Davidson <william11231999@gmail.com>
2023-01-10 22:43:09 -0500

    Navbar and Login Work

---
+++ portfolio/src/components/LoginPopup.vue
@@ -0,0 +1,55 @@
+<template>
+        <div class="modal-dialog">
+            <div class="modal-content" style="width= 600px; height= 400px;">
+                <div class="modal-header" style="border-color: black;">
+                    <h5 class="modal-title ms-auto" style="color: black">Login</h5>
+                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Login-Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form @submit.prevent="handleLogin">
+                        <div class="row my-2">
+                            <div class="col" style="width: 200 px;">
+                                <label for="LoginUsername" class="col-form-labels" style="color: black">Username/Email:</label>
+                            </div>
+                            <div class="col">
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="row my-2">
+                            <div class="col">
+                                <label for="LoginPassword" class="col-form-labels" style="color: black">Password:</label>
+                            </div>
+                            <div class="col">
+                                <input type="password">
+                            </div>
+                        </div>
+                        <button class="btn btn-secondary mt-2" style="color: black;">Login</button>
+                    </form>
+                    <div class="mt-3">
+                        Don't have an account? Create one for free!
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+        const handleLogin = {
+
+        }
+
+        return { handleLogin }
+    }
+
+}
+</script>
+
+<style>
+.modal {
+    --bs-modal-width: 400px;
+    --bs-modal-bg: rgb(146, 143, 143);
+}
+</style>
--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -1,25 +1,45 @@
 <template>
-  <nav class="container-fluid fixed-top navbar navbar-default navbar-expand-md mt-0 mb-5 p-0">
-    <div class="container-fluid ps-0 pe-0">
-        <div class="me-1">
-            <router-link :to="{name: 'Home'}" class="navbar-header navbar-brand link p-2 m-0" style="text-decoration: none; font-size: 24px;">Logo</router-link>
+    <nav class="fixed-top navbar navbar-default navbar-expand-md mt-0 mb-5 p-0">
+        <div class="container-fluid ps-1 pe-1">
+            <div class="me-1">
+                <router-link :to="{name: 'Home'}" class="navbar-header navbar-brand link p-2 m-0" style="text-decoration: none; font-size: 24px;">Logo</router-link>
+            </div>
+            <div class="nav navbar-nav me-auto">
+                <router-link :to="{name: 'Projects'}" class="mx-2 link" style="text-decoration: none; color: #fff">Projects</router-link> 
+                <router-link :to="{name: 'Announcements'}" class="mx-2 link" style="text-decoration: none; color: #fff">Announcements</router-link>
+            </div>
+            <div class="nav navbar-nav ms-auto">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <button class="btn" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-bars"></i>
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end" style="background-color: rgb(146, 143, 143);">
+                                    <li><button class="dropdown-item btn" type="button" data-bs-toggle="modal" data-bs-target="#LoginModal">Login</button></li>
+                                    <li><button class="dropdown-item btn" type="button">Settings</button></li>
+                                    <li><button class="dropdown-item btn" type="button">Report Bug</button></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="nav navbar-nav me-auto">
-            <router-link :to="{name: 'Projects'}" class="mx-2 link" style="text-decoration: none; color: #fff">Projects</router-link> 
-            <router-link :to="{name: 'Announcements'}" class="mx-2 link" style="text-decoration: none; color: #fff">Announcements</router-link>
-        </div>
-        <div class="nav navbar-nav ms-auto">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
-                <i class="fa-solid fa-bars">-</i>
-            </button>
-        </div>
+    </nav>
+    <div id="LoginModal" class="modal fade">
+        <LoginPopup/>
     </div>
-  </nav>
 </template>
 
 <script>
+import LoginPopup from "./LoginPopup.vue";
 export default {
-
+    components: { LoginPopup },
+    setup() {
+        
+    }
 }
 </script>
 
@@ -28,5 +48,8 @@ nav {
     background-color: rgb(146, 143, 143);
     padding: 0px;
     height: 50px;
+}
+.dropdown-menu {
+    --bs-dropdown-link-hover-bg: rgb(201, 201, 201);
 }
 </style>
